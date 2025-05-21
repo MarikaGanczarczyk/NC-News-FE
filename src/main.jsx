33067abe9809cf2a9fb9e9f847356067f41cf5e8
Marika <marikabartman27@gmail.com>
2025-05-21 09:54:55 +0100

    5-individual article

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
-import NavBar from "./Components/NavBar.jsx";
+import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
