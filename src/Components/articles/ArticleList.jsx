e96390491eaf42a0f3ca33643cb8f66dd89f9f1f
Marika <marikabartman27@gmail.com>
2025-10-25 11:08:40 +0100

    Removed sort by from Article List

--- src/Components/articles/ArticleList.jsx
+++ src/Components/articles/ArticleList.jsx
@@ -7,21 +7,20 @@ import './Articles.css'
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [sortBy, setSortBy] = useState("");
-  const [order, setOrder] = useState("asc");
+
 
   useEffect(() => {
     setIsLoading(true);
-    getArticles({ sortBy: sortBy || undefined, order })
-      .then((fetchedArticles) => {
-        setArticles(fetchedArticles);
+    getArticles()
+      .then((response) => {
+        setArticles(response);
         setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
         setIsLoading(false);
       });
-  }, [sortBy, order]);
+  }, []);
 
   if (isLoading) {
     return <p className="loading">Loading...</p>;
@@ -31,25 +30,6 @@ const ArticleList = () => {
     <>
     <div className="article-container">
       <h1 className="articles-header-title">Articles</h1>
-
-      <div className="sorting-controls">
-        <label>Sort by: </label>
-        <select  value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="date">Date</option>
-          <option value="likes">Likes</option>
-          <option value="comment_count">Comments</option>
-        </select>
-        <label>Order: </label>
-        <select
-          value={order}
-          onChange={(e) => setOrder(e.target.value)}
-        
-        >
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
-      </div>
-
       <ul className="article-list">
         {articles.map((article) => (
           <li key={article.article_id}>
